2c777faa3c08c9a9a64293e8f52fbd7d3e823819
pugur <pugurmc@gmail.com>
2026-07-11 21:59:25 +0900

    refactor: change item default value to air

--- src/main/java/com/pug523/shelf/ShelfConfig.java
+++ src/main/java/com/pug523/shelf/ShelfConfig.java
@@ -9,7 +9,7 @@ import java.io.Serializable;
 public class ShelfConfig implements Serializable {
     public boolean autoEat = false;
     public boolean autoRestock = false;
-    public Item restockItem = null;
+    public Item restockItem = net.minecraft.world.item.Items.AIR;
     public int fov = 90;
     public int renderDistance = 32;
     public double volume = 100.0d;
