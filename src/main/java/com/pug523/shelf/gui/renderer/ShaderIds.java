149a2b870ecdfd21e146f059260940f7d4c23d06
pugur <pugurmc@gmail.com>
2026-07-11 21:54:46 +0900

    feat: add shader ids reference class

---
+++ src/main/java/com/pug523/shelf/gui/renderer/ShaderIds.java
@@ -0,0 +1,12 @@
+package com.pug523.shelf.gui.renderer;
+
+import com.pug523.shelf.compat.IdentifierCompat;
+
+import net.minecraft.resources.Identifier;
+
+public class ShaderIds {
+    private ShaderIds() {
+    }
+
+    public static final Identifier SDF = IdentifierCompat.ofShelf("sdf");
+}
