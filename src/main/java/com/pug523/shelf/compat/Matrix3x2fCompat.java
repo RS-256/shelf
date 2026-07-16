35b7ac6b5270ea941e615293a500b99527538313
pugur <pugurmc@gmail.com>
2026-07-17 07:15:37 +0900

    feat: add matrix3x2f / pose stack compatible layer

---
+++ src/main/java/com/pug523/shelf/compat/Matrix3x2fCompat.java
@@ -0,0 +1,47 @@
+package com.pug523.shelf.compat;
+
+//#if MC >= 12005
+import org.joml.Matrix3x2f;
+import org.joml.Matrix3x2fc;
+//#else
+//$$ import com.mojang.blaze3d.vertex.PoseStack;
+//#endif
+
+public class Matrix3x2fCompat {
+    //#if MC >= 12005
+    public final Matrix3x2fc pose;
+    //#else
+    //$$ public final PoseStack.Pose pose;
+    //#endif
+
+    // @formatter:off
+    //#if MC >= 12005
+    public Matrix3x2fCompat(Matrix3x2fc pose) {
+    //#else
+    //$$ public Matrix3x2fCompat(Matrix3x2f pose) {
+    //#endif
+    // @formatter:on
+        this.pose = pose;
+    }
+
+    public Matrix3x2fCompat copy() {
+        return new Matrix3x2fCompat(pose);
+    }
+
+    // @formatter:off
+    //#if MC >= 12005
+    public static Matrix3x2fCompat copy(Matrix3x2fc src) {
+    //#else
+    //$$ public static Matrix3x2fCompat copy(PoseStack src) {
+    //#endif
+    // @formatter:on
+        //#if MC >= 12106
+        return new Matrix3x2fCompat(new Matrix3x2f(src));
+        //#elseif MC >= 12005
+        //$$ return new Matrix3x2fCompat(src.last().copy());
+        //#else
+        //$$ PoseStack.Pose p = src.last();
+        //$$ return new Matrix3x2fCompat(new PoseStack.Pose(p.pose(), p.normal()));
+        //#endif
+    }
+}
